49e13707c1a0aa50340654f7877c4e402d6ff56e
tshan <tshan@byulsoft.com>
2020-05-28 21:21:18 +0900

    작업 중

---
+++ lib/models/user.dart
@@ -0,0 +1,26 @@
+import 'package:scoped_model/scoped_model.dart';
+
+class UserModel extends Model {
+  int _user_seq;
+  String _id, _name, _smoker_yn, _manager_yn, _token;
+
+  int get user_seq { return _user_seq; }
+  String get id { return _id; }
+  String get name { return _name; }
+  String get smoker_yn { return _smoker_yn; }
+  String get manager_yn { return _manager_yn; }
+  String get token { return _token; }
+
+  UserModel({user_seq, id, name, smoker_yn, manager_yn, token});
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      user_seq: json['user_seq'],
+      id: json['id'],
+      name: json['name'],
+      smoker_yn: json['smoker_yn'],
+      manager_yn: json['manager_yn'],
+      token: json['token'],
+    );
+  }
+}
---
+++ lib/screen_login.dart
@@ -0,0 +1,174 @@
+import 'package:chooselunch/models/user.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:localstorage/localstorage.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import 'dart:convert';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  final LocalStorage storage = new LocalStorage('ChooseLunchLocalStorage');
+
+  final TextEditingController _idController = new TextEditingController();
+  final TextEditingController _passwordController = new TextEditingController();
+  String _id = "";
+  String _password = "";
+  bool _remember = false;
+
+  _LoginPageState() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: _buildBar(context),
+      body: new Container(
+        padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 80.0),
+        child: new Column(
+          children: <Widget>[
+            _buildTextFields(),
+            _buildCheckbox(),
+            _buildButtons(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBar(BuildContext context) {
+    return new AppBar(
+      title: new Text("Choose Lunch Login"),
+      centerTitle: true,
+    );
+  }
+
+  Widget _buildTextFields() {
+    return new Container(
+      child: new Column(
+        children: <Widget>[
+          new Container(
+            child: new TextField(
+              autofocus: true,
+              controller: _idController,
+              decoration: new InputDecoration(
+                  labelText: 'ID'
+              ),
+            ),
+          ),
+          new Container(
+            child: new TextField(
+              controller: _passwordController,
+              decoration: new InputDecoration(
+                  labelText: 'Password'
+              ),
+              obscureText: true,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButtons() {
+    return new Container(
+      padding: EdgeInsets.only(top: 30.0),
+      child: new Column(
+        children: <Widget>[
+          new RaisedButton(
+            child: new Text('Login'),
+            onPressed: _loginPressed,
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCheckbox() {
+      return new Container(
+        padding: EdgeInsets.only(top: 30.0),
+        child: new Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Checkbox(
+              value: _remember,
+              onChanged: _onRememberChanged,
+            ),
+            new Text('로그인 유지')
+          ],
+        ),
+      );
+  }
+
+  Future<void> _loginPressed () async {
+    _id = _idController.text;
+    _password = _passwordController.text;
+    final response = await http.post(
+      'http://cl.byulsoft.com/api/login',
+      body: jsonEncode(
+        {
+          'id': _id,
+          'password': _password,
+          'mobileYn': 'Y'
+        },
+      ),
+      headers: {'Content-Type': "application/json"},
+    );
+
+    print('아 뭔디');
+    print(response.body);
+
+    // ignore: unrelated_type_equality_checks
+    if(response.body == 'false') {
+      _showDialog();
+    } else {
+      final UserModel _user = UserModel.fromJson(jsonDecode(response.body));
+      if(_remember) {
+        storage.setItem('ChooseLunchRembmerToken', _user.token);
+        storage.setItem('ChooseLunchRembmerId', _user.id);
+      }
+      print('logged user : $_user');
+    }
+  }
+
+  void _onRememberChanged(bool newValue) => setState(() {
+    _remember = newValue;
+  });
+
+  void _showDialog() {
+    /*
+    Fluttertoast.showToast(
+        msg: "로그인 실패 !!",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0
+    );
+     */
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("로그인 실패 !!"),
+          content: new Text("ID 혹은 비밀번호를 확인해주세요."),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text("Close"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
